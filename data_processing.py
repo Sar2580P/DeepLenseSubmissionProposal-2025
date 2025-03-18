4134d8a1378374f8bb872feb565df829c9266ae6
Sar2580P <sporwal1818@gmail.com>
2025-03-18 11:17:10 +0530

    data splitting strategies for different tasks

---
+++ data_processing.py
@@ -0,0 +1,121 @@
+import os
+import glob
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def common_classification_dataset(data_dir: str, save_dir:str, split_ratio: float = 0.1) -> None:
+    label0_files = glob.glob(os.path.join(data_dir, 'no', '*.npy'))
+    label1_files = glob.glob(os.path.join(data_dir, 'sphere', '*.npy'))
+    label2_files = glob.glob(os.path.join(data_dir, 'vort', '*.npy'))
+    
+    # Split data into train, val, test
+    label0_train, label0_test = train_test_split(label0_files, test_size=split_ratio, random_state=42, shuffle=True)
+    label1_train, label1_test = train_test_split(label1_files, test_size=split_ratio, random_state=42, shuffle=True)
+    label2_train, label2_test = train_test_split(label2_files, test_size=split_ratio, random_state=42, shuffle=True)
+    
+    label0_train, label0_val = train_test_split(label0_train, test_size=split_ratio, random_state=42, shuffle=True)
+    label1_train, label1_val = train_test_split(label1_train, test_size=split_ratio, random_state=42, shuffle=True)
+    label2_train, label2_val = train_test_split(label2_train, test_size=split_ratio, random_state=42, shuffle=True)
+    
+    # Create dataframes
+    train_df = pd.DataFrame({'image_path': label0_train + label1_train + label2_train, 
+                             'label': [0]*len(label0_train) + [1]*len(label1_train) + [2]*len(label2_train)}).sample(frac=1).reset_index(drop=True)
+    val_df = pd.DataFrame({'image_path': label0_val + label1_val + label2_val,
+                           'label': [0]*len(label0_val) + [1]*len(label1_val) + [2]*len(label2_val)}).sample(frac=1).reset_index(drop=True)
+    test_df = pd.DataFrame({'image_path': label0_test + label1_test + label2_test,
+                            'label': [0]*len(label0_test) + [1]*len(label1_test) + [2]*len(label2_test)}).sample(frac=1).reset_index(drop=True)
+    
+    # Save dataframes
+    dir = os.path.join(save_dir, 'common_classification_dataset')
+    os.makedirs(dir, exist_ok=True)
+    train_df.to_csv(os.path.join(dir, 'train_df.csv'), index=False)
+    val_df.to_csv(os.path.join(dir, 'val_df.csv'), index=False)
+    test_df.to_csv(os.path.join(dir, 'test_df.csv'), index=False)
+
+    return
+
+def diffusion_dataset(save_dir:str):
+    SOURCE_DIR = 'data/Samples'
+
+    files = glob.glob(os.path.join(SOURCE_DIR, '*.npy'))
+    train_files, test_files = train_test_split(files, test_size=0.1, random_state=42, shuffle=True)
+    train_files, val_files = train_test_split(train_files, test_size=0.1, random_state=42, shuffle=True)
+    
+    # save files as 'img_path' col in dataframe
+    train_df = pd.DataFrame({'img_path': train_files})
+    val_df = pd.DataFrame({'img_path': val_files})
+    test_df = pd.DataFrame({'img_path': test_files})
+    
+    # save dataframes
+    dir = os.path.join(save_dir, 'diffusion_dataset')
+    os.makedirs(dir, exist_ok=True)
+    train_df.to_csv(os.path.join(dir, 'train_df.csv'), index=False)
+    val_df.to_csv(os.path.join(dir, 'val_df.csv'), index=False)
+    test_df.to_csv(os.path.join(dir, 'test_df.csv'), index=False)
+    
+    return
+
+def foundation_models_pretraining_dataset(save_dir:str):
+    
+    SOURCE_DIR = 'data/Dataset/no_sub'
+    files = glob.glob(os.path.join(SOURCE_DIR, '*.npy'))
+    train_files, test_files = train_test_split(files, test_size=0.1, random_state=42, shuffle=True)
+    train_files, val_files = train_test_split(train_files, test_size=0.1, random_state=42, shuffle=True)
+    
+    # save files as 'img_path' col in dataframe
+    train_df = pd.DataFrame({'img_path': train_files})
+    val_df = pd.DataFrame({'img_path': val_files})
+    test_df = pd.DataFrame({'img_path': test_files})
+    
+    # save dataframes
+    dir = os.path.join(save_dir, 'foundation_models_pretraining_dataset')
+    os.makedirs(dir, exist_ok=True)
+    train_df.to_csv(os.path.join(dir, 'train_df.csv'), index=False)
+    val_df.to_csv(os.path.join(dir, 'val_df.csv'), index=False)
+    test_df.to_csv(os.path.join(dir, 'test_df.csv'), index=False)
+    
+    return
+
+def foundation_models_classification_dataset(save_dir:str):
+    
+    SOURCE_DIR = 'data/Dataset'
+    
+    no_sub_files = glob.glob(os.path.join(SOURCE_DIR, 'no_sub', '*.npy'))   
+    cdm_files = glob.glob(os.path.join(SOURCE_DIR, 'cdm', '*.npy'))
+    axion_files = glob.glob(os.path.join(SOURCE_DIR, 'axion', '*.npy'))
+    
+    # Split data into train, val, test
+    no_sub_train, no_sub_test = train_test_split(no_sub_files, test_size=0.1, random_state=42, shuffle=True)
+    cdm_train, cdm_test = train_test_split(cdm_files, test_size=0.1, random_state=42, shuffle=True)
+    axion_train, axion_test = train_test_split(axion_files, test_size=0.1, random_state=42, shuffle=True)
+    
+    no_sub_train, no_sub_val = train_test_split(no_sub_train, test_size=0.1, random_state=42, shuffle=True)
+    cdm_train, cdm_val = train_test_split(cdm_train, test_size=0.1, random_state=42, shuffle=True)  
+    axion_train, axion_val = train_test_split(axion_train, test_size=0.1, random_state=42, shuffle=True)
+    
+    # Create dataframes
+    train_df = pd.DataFrame({'image_path': no_sub_train + cdm_train + axion_train, 
+                             'label': [0]*len(no_sub_train) + [1]*len(cdm_train) + [2]*len(axion_train)}).sample(frac=1).reset_index(drop=True)
+    val_df = pd.DataFrame({'image_path': no_sub_val + cdm_val + axion_val,
+                           'label': [0]*len(no_sub_val) + [1]*len(cdm_val) + [2]*len(axion_val)}).sample(frac=1).reset_index(drop=True)
+    test_df = pd.DataFrame({'image_path': no_sub_test + cdm_test + axion_test,
+                            'label': [0]*len(no_sub_test) + [1]*len(cdm_test) + [2]*len(axion_test)}).sample(frac=1).reset_index(drop=True)
+    
+    # save dataframes
+    dir = os.path.join(save_dir, 'foundation_models_classification_dataset')
+    os.makedirs(dir, exist_ok=True)
+    train_df.to_csv(os.path.join(dir, 'train_df.csv'), index=False)
+    val_df.to_csv(os.path.join(dir, 'val_df.csv'), index=False)    
+    test_df.to_csv(os.path.join(dir, 'test_df.csv'), index=False)
+    
+    return
+
+
+if __name__=="__main__":
+    save_dir = 'data/dataframes'
+    common_classification_dataset('data/dataset/train', save_dir)
+    diffusion_dataset(save_dir)
+    foundation_models_pretraining_dataset(save_dir)
+    foundation_models_classification_dataset(save_dir)
+    
+    print('Datasets created successfully!')
--- diffusion/dataloader.py
+++ diffusion/dataloader.py
@@ -2,10 +2,10 @@ from torch.utils.data import Dataset, DataLoader
 import os
 import numpy as np
 from functools import partial
+import pandas as pd
 
 class DiffusionDataset(Dataset):
-    def __init__(self, data_dir:str, total_samples:int, 
-                 mode:str='train' , split_ratio:float=0.75,  transform=None):
+    def __init__(self, data_csv_path:str, transform=None):
         """
         Custom PyTorch Dataset
         
@@ -13,44 +13,14 @@ class DiffusionDataset(Dataset):
             data_path (str): Path to the data file
             transform (callable, optional): Optional transform to be applied to samples
         """
-        self.total_samples = total_samples
-        self.mode = mode
-        self.transform = transform
-        self.data_dir = data_dir
-        
-        self.data_samples_ct , self.data = self.get_data_splits(split_ratio)
-        
-    def get_data_splits(self, split_ratio:float):
-        """
-        Utility function to split the data into train and test
-        
-        Args:
-            split_ratio (float): Ratio to split the data
-        """
-        np.random.seed(42)
-        train_samples = int(self.total_samples*split_ratio)
-        val_samples = (self.total_samples - train_samples)//2
-        test_samples = self.total_samples - train_samples - val_samples
-        
-        train_data = np.random.choice(self.total_samples, train_samples, replace=False)
-        rem_data = np.setdiff1d(np.arange(self.total_samples), train_data)
-        val_data = np.random.choice(rem_data, val_samples, replace=False)
-        test_data = np.setdiff1d(rem_data, val_data)
-        
-        if self.mode == 'train':
-            return train_samples , train_data
-        elif self.mode == 'val':
-            return val_samples, val_data
-        elif self.mode == 'test':
-            return test_samples, test_data
-        else:
-            raise ValueError("Invalid mode for DiffusionDataset... Please choose from ['train', 'val', 'test']")
-        
+        self.transform = transform     
+        self.df = pd.read_csv(data_csv_path)   
+    
     def __len__(self):
-        return self.data_samples_ct
+        return len(self.df)
     
     def __getitem__(self, idx):
-        img_path = os.path.join(self.data_dir, f'sample{self.data[idx]}.npy')
+        img_path = self.df.iloc[idx]['img_path']
         img = np.load(img_path)
         
         if self.transform:
@@ -66,11 +36,10 @@ def get_dataloaders(data_config:dict):
     Args:
         data_config (dict): Configuration dictionary for data
     """
-    dataset = partial(DiffusionDataset, data_dir=data_config['data_dir'], 
-                      total_samples=data_config['total_samples'], split_ratio=data_config['split_ratio'])
     
-    train_dataset, val_dataset,  test_dataset = dataset(mode="train") , dataset(mode="val") , dataset(mode="test")
-    
+    train_dataset = DiffusionDataset(data_csv_path=data_config['train_path'])
+    val_dataset = DiffusionDataset(data_csv_path=data_config['val_path'])
+    test_dataset = DiffusionDataset(data_csv_path=data_config['test_path'])
     train_loader = DataLoader(train_dataset, batch_size=data_config['BATCH_SIZE'],
                               shuffle=True, num_workers=data_config['num_workers'])
     val_loader = DataLoader(val_dataset, batch_size=data_config['BATCH_SIZE'],
