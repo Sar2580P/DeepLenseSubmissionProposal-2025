c148abd6551bb20ba8175065414f736869cb40d4
Sar2580P <sporwal1818@gmail.com>
2025-03-27 21:42:46 +0530

    added transformations

--- diffusion/dataloader.py
+++ diffusion/dataloader.py
@@ -9,12 +9,15 @@ transform = T.Compose([
   #  T.ToPILImage(),          # Convert numpy array (150x150) to PIL Image.
     T.ToTensor() ,
     T.Pad(padding=1),        # Pad 1 pixel on all sides to get 152x152.
+    T.RandomRotation(degrees=15),
+    T.RandomHorizontalFlip(),
+    T.RandomVerticalFlip(),
    # T.ToTensor()             # Convert back to tensor.
 ])
 
 
 class DiffusionDataset(Dataset):
-    def __init__(self, data_csv_path:str, transform=transform):
+    def __init__(self, data_csv_path:str, transform=None):
         """
         Custom PyTorch Dataset
         
@@ -22,7 +25,7 @@ class DiffusionDataset(Dataset):
             data_path (str): Path to the data file
             transform (callable, optional): Optional transform to be applied to samples
         """
-        self.transform = transform     
+        self.transform = transform or T.Compose([T.ToTensor() ,  T.Pad(padding=1)])
         self.df = pd.read_csv(data_csv_path)   
     
     def __len__(self):
@@ -46,7 +49,7 @@ def get_dataloaders(data_config:dict):
         data_config (dict): Configuration dictionary for data
     """
     
-    train_dataset = DiffusionDataset(data_csv_path=data_config['train_path'])
+    train_dataset = DiffusionDataset(data_csv_path=data_config['train_path'], transform=transform)
     val_dataset = DiffusionDataset(data_csv_path=data_config['val_path'])
     test_dataset = DiffusionDataset(data_csv_path=data_config['test_path'])
     train_loader = DataLoader(train_dataset, batch_size=data_config['BATCH_SIZE'],
