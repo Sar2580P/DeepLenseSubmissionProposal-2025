fbfe2209c44bc6235dfd49ef231435702472890b
Sar2580P <sporwal1818@gmail.com>
2025-03-28 19:43:36 +0530

    diffusion sweep

--- scripts/diffusion.sh
+++ scripts/diffusion.sh
@@ -25,6 +25,7 @@ export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 ## Launch the training script with srun, which spawns 2 tasks in parallel
 #srun --ntasks=2 --cpus-per-task=10 python diffusion/trainer.py  > logs/diffusion_training.log 2>&1
 
+srun --ntasks=2 --cpus-per-task=10 python diffusion/sweep/sweep.py  > logs/diffusion_sweep.log 2>&1
 python diffusion/fid_eval.py > logs/diffusion_fid_eval.log 2>&1
 
 # Optional: Detach from the script completely
